e24a6080b0a419820091e53e9a6613cb1895032d
PadmaGnanapriya <padmaisuru@gmail.com>
2021-12-25 13:19:15 +0530

    setup route paths constance and sass styles.

---
+++ src/constants/RoutePaths.ts
@@ -0,0 +1,26 @@
+
+export const ROUTE_PATH = {
+  HOME: '/',
+  ABOUT: '/about',
+  CONTACT: '/contact',
+  CATEGORIES: '/categories',
+  FIND_SERVICE: '/find-service',
+  SERVICE_SUPPLIER_OVERVIEW: '/service-supplier-overview',
+  PAGE_NOT_FOUND: 'page-not-found',
+  FAQ: '/faq',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: 'reset-password',
+
+  RE_NEW_MEMBERSHIP: '/re-new-membership',
+  PROFILE: '/profile',
+  NOTIFICATION_MANAGEMENT: '/notification-management',
+
+  DASHBOARD: '/dashboard',
+  MESSAGE_MANAGEMENT: '/message-management',
+  SERVICE_SUPPLIER_MANAGEMENT: '/service-supplier-management',
+  INCOME_MANAGEMENT: '/income-management',
+  FAQ_MANAGEMENT: 'faq-management',
+  TERMS_AND_CONDITIONS: '/terms-and-conditions',
+}
