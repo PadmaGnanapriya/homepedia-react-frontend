ebda3352fadba01e13fed307d62d119196dfb730
PadmaGnanapriya <padmaisuru@gmail.com>
2021-12-26 16:07:13 +0530

    Create income management page UI.

---
+++ src/components/CustomTable.tsx
@@ -0,0 +1,54 @@
+import React, {forwardRef} from "react";
+import MaterialTable from "material-table";
+import {Container} from "react-bootstrap";
+
+type CustomTableProps = {
+  title: string;
+  columns: any;
+  data: any;
+  actions: any;
+}
+
+const CustomTable: React.FC<CustomTableProps> = (props) => {
+
+  const tableIcons: any = {
+    Add: forwardRef((props, ref) => <span className="icon-add" {...props}  />),
+    Check: forwardRef((props, ref) => <span className="icon-check" {...props}  />),
+    Clear: forwardRef((props, ref) => <span className="icon-add" {...props}  />),
+    Delete: forwardRef((props, ref) => <span className="icon-clear" {...props}  />),
+    DetailPanel: forwardRef((props, ref) => <span className="icon-delete" {...props}  />),
+    Edit: forwardRef((props, ref) => <span className="icon-edit" {...props}  />),
+    ResetSearch: forwardRef((props, ref) => <span className="icon-refresh" {...props}  />),
+    Search: forwardRef((props, ref) => <span className="icon-search" {...props}  />),
+    Export: forwardRef((props, ref) => <span className="" {...props}  />),
+    Filter: forwardRef((props, ref) => <span className="icon-filter_list" {...props}  />),
+    FirstPage: forwardRef((props, ref) => <span className="icon-skip_previous" {...props}  />),
+    LastPage: forwardRef((props, ref) => <span className="icon-skip_next" {...props}  />),
+    PreviousPage: forwardRef((props, ref) => <span className="icon-keyboard_arrow_left" {...props}  />),
+    SortArrow: forwardRef((props, ref) => <span className="icon-sort-amount-asc" {...props}  />),
+    ThirdStateCheck: forwardRef((props, ref) => <span className="" {...props}  />),
+    ViewColumn: forwardRef((props, ref) => <span className="icon-view_column" {...props}  />),
+    NextPage: forwardRef((props, ref) => <span className="icon-keyboard_arrow_right" {...props}  />),
+  };
+
+  const tableRef = React.createRef();
+
+  return (
+    <Container className="py-5">
+      <MaterialTable
+        icons={tableIcons}
+        title={props.title}
+        tableRef={tableRef}
+        options={{
+          actionsColumnIndex: -1,
+          addRowPosition: 'first'
+        }}
+        columns={props.columns}
+        data={props.data}
+        actions={props.actions}
+      />
+    </Container>
+  )
+}
+
+export default CustomTable;
--- src/views/IncomeManagement.tsx
+++ src/views/IncomeManagement.tsx
@@ -1,8 +1,92 @@
-import React from "react";
+import React, {forwardRef, useState} from "react";
+import CustomTable from "../components/CustomTable";
+import {Button, Modal, Image} from "react-bootstrap";
+import paymentSlip from "../assets/images/paymentSlip.jpg";
 
 const IncomeManagement: React.FC = () => {
+    const data = [
+        {
+            time: '23/11/2021 2.00PM',
+            fromAccountNo: 'XXXX XXXX 1516',
+            transferAmount: 'LKR 4,000.00',
+            description: 'from Sunimal to 6 month membership',
+            paymentMethod: 'Online Banking',
+            status: 'Authorized'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            fromAccountNo: 'XXXX XXXX 1516',
+            transferAmount: 'LKR 4,000.00',
+            description: 'from Kamal to 6 month membership',
+            paymentMethod: 'Online Banking',
+            status: 'Authorized'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            fromAccountNo: 'XXXX XXXX 1516',
+            transferAmount: 'LKR 7,000.00',
+            description: 'from Nimal to 12 month membership',
+            paymentMethod: 'Online Banking',
+            status: 'Authorized'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            fromAccountNo: 'XXXX XXXX 1516',
+            transferAmount: 'LKR 13,000.00',
+            description: 'from Amal to 24 month membership',
+            paymentMethod: 'Online Banking',
+            status: 'Authorized'
+        },
+
+
+    ];
+
+    const columns = [
+        {title: 'Time', field: 'time'},
+        {title: 'From', field: 'fromAccountNo'},
+        {title: 'Amount', field: 'transferAmount'},
+        {title: 'Description', field: 'description'},
+        {title: 'Payment Method', field: 'paymentMethod'},
+        {title: 'Status', field: 'status'}
+
+    ];
+
+    // TODO : Create function for onClick event. remove dummy console log
+    const actions: any = [
+        {
+            icon: forwardRef((props, ref) => <span className="icon-attach_file" {...props}  />),
+            tooltip: 'Preview Payment',
+            onClick: (event: any, rowData: any) => setModalShow(true)
+        },
+        {
+            icon: forwardRef((props, ref) => <span className="icon-send" {...props}  />),
+            tooltip: 'Send activation Message',
+            onClick: (event: any, rowData: any) => console.log("You saved ", rowData)
+        },
+    ];
+    const [modalShow, setModalShow] = useState(false);
+
+
     return (
-        <div>Income Management</div>
+      <React.Fragment>
+          <CustomTable title={'Income Management'} data={data} columns={columns} actions={actions}/>
+
+          <Modal
+            show={modalShow}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+              <Modal.Body>
+                  <h4 className="text-center">Payment Overview</h4>
+
+                  <Image className="img-fluid" src={paymentSlip}/>
+              </Modal.Body>
+              <Modal.Footer>
+                  <Button onClick={() => setModalShow(false)}>Close</Button>
+              </Modal.Footer>
+          </Modal>
+      </React.Fragment>
     )
 }
 
