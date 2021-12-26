0781459866307649bb1686b0e587833102b784b1
PadmaGnanapriya <padmaisuru@gmail.com>
2021-12-26 18:38:12 +0530

    Create find service page UI.

--- src/views/FindService.tsx
+++ src/views/FindService.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import {Container, Dropdown, DropdownButton, Row} from "react-bootstrap";
+import ServiceSupplierCard from "../components/ServiceSupplierCard";
+import {cities, serviceTypes, suppliers} from "../store/dummyData";
 
 const FindService: React.FC = () => {
+    const [activeLinkIndex, setActiveLinkIndex] = useState(0);
+
+    const handleOnClickSideNavLink = (index: number) => {
+        setActiveLinkIndex(index)
+    }
     return (
-        <div>FindService</div>
+      <div className="d-flex">
+          <div style={{width: '200px'}} className="py-3 side-nav-container">
+              <DropdownButton id="dropdown-basic-button" title="  City  ">
+                  {
+                      cities.map(element =>  <Dropdown.Item href="#/action-1">{element}</Dropdown.Item>)
+                  }
+              </DropdownButton>
+              {
+                  serviceTypes.map((serviceType, index) =>
+                    <div onClick={() => handleOnClickSideNavLink(index)} key={index}
+                         className={activeLinkIndex === index ? 'side-link active py-2 px-4' : 'side-link py-2 px-4'}><span
+                      className={serviceType.icon}/> {serviceType.label}</div>)
+              }
+          </div>
+
+          <Container fluid={true} className="py-4 px-3" style={{
+              background: 'radial-gradient(circle, rgba(26,8,69,1) 0%, rgba(43,19,98,1) 35%, rgba(50,121,189,1) 100%)'
+          }}>
+              <Row>
+                  {
+                      suppliers.map(supplier => <ServiceSupplierCard supplier={supplier}/>)
+                  }
+              </Row>
+          </Container>
+
+
+      </div>
     )
 }
-
 export default FindService;
--- src/views/NotificationManagement.tsx
+++ src/views/NotificationManagement.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, {forwardRef} from "react";
+import CustomTable from "../components/CustomTable";
 
-const ServiceSupplierManagement: React.FC = () => {
+const NotificationManagement: React.FC = () => {
+    const data = [
+        {
+            time: '23/11/2021 2.00PM',
+            email: 'padmaisuru@gmail.com',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat bibendum metus. Quisque sed nisi accumsan'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            email: 'padmaisuru@gmail.com',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. . Ut placerat bibendum metus. Quisque sed nisi accumsan'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            email: 'padmaisuru@gmail.com',
+            message: 'Ut placerat bibendum metus. Quisque sed nisi accumsan'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            email: 'padmaisuru@gmail.com',
+            message: 'Lorem ipsum dolor sit amet, iam, vitae semper tellus luctus eget. Ut placerat bibendum metus. Quisque sed nisi accumsan'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            email: 'padmaisuru@gmail.com',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat bibendum metus. Quisque sed nisi accumsan'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            email: 'padmaisuru@gmail.com',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit tin. Ut placerat bibendum metus. Quisque sed nisi accumsan'
+        },
+        {
+            time: '23/11/2021 2.00PM',
+            email: 'padmaisuru@gmail.com',
+            message: 'Demo test Message'
+        },
+
+    ];
+
+    const columns = [
+        {title: 'Time', field: 'time'},
+        {title: 'Email', field: 'email'},
+        {title: 'Message', field: 'message'},
+
+    ];
+
+    // TODO : Create function for onClick event. remove dummy console log
+    const actions: any = [
+        {
+            icon: forwardRef((props, ref) => <span className="icon-send" {...props}  />),
+            tooltip: 'Delete this Notification',
+            onClick: (event: any, rowData: any) => console.log("You saved ", rowData)
+        },
+        {
+            icon: forwardRef((props, ref) => <span className="icon-delete" {...props}  />),
+            tooltip: 'Delete this Notification',
+            onClick: (event: any, rowData: any) => console.log("You saved ", rowData)
+        }
+    ]
+
     return (
-        <div>Service Supplier Management</div>
+      <CustomTable title={'Notification Management'} data={data} columns={columns} actions={actions}/>
     )
 }
 
-export default ServiceSupplierManagement;
+export default NotificationManagement;
