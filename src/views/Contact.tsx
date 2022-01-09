d56538a0d05b5e3f002af4f4b6335907da273fb0
ChamodiJayodya <jayodyachamodi@gmail.com>
2022-01-09 20:54:49 +0530

    Create contact us UI

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React from 'react';
+import image from "../assets/images/page-contact.webp";
+import BackgroundImage from "../components/BackGroundImg";
+import {Button, Card, Form, Image} from "react-bootstrap";
+import logo from "../assets/images/logo.webp";
 
 const Contact: React.FC = () => {
+
     return (
-        <div>Contact</div>
-    )
+      <BackgroundImage image={image}>
+
+          <Card className="card-div p-3">
+              <Image className="m-auto" width={180} src={logo}/>
+              <div className="text-center my-2">
+                  <h1 className="h4">Contact Us</h1>
+                  <small>Lets discuss about your problem with us.</small>
+              </div>
+
+              <Form>
+                  <Form.Group className="mb-3" controlId="formGroupName">
+                      {/*<Form.Label>Full Name</Form.Label>*/}
+                      <Form.Control type="text" placeholder="Full Name"/>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formGroupEmail">
+                      {/*<Form.Label>Email address</Form.Label>*/}
+                      <Form.Control type="email" placeholder="Enter email"/>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formGroupMessage">
+                      {/*<Form.Label>Message</Form.Label>*/}
+                      <Form.Control as="textarea" aria-label="With textarea" placeholder="Your Message" />
+                  </Form.Group>
+
+
+                  <div className="text-center">
+                      <Button type="submit" className="px-5 my-3">Send</Button>
+                  </div>
+              </Form>
+
+          </Card>
+
+      </BackgroundImage>
+    );
 }
 
 export default Contact;
