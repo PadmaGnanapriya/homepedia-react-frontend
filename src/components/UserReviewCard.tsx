f6cb317717161ebe87fc9b323625904bac1cd8e8
ChamodiJayodya <jayodyachamodi@gmail.com>
2022-01-13 19:56:41 +0530

    Create service supplier overview page UI

---
+++ src/components/UserReviewCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Col, Container, Row, Image, Card} from "react-bootstrap";
+import userPlaceHolder from "../assets/images/userPlaceHolder.webp";
+// @ts-ignore
+import ReactStars from "react-rating-stars-component";
+
+type UserReviewCard = {
+  id: number;
+  rating: number;
+  comment: string;
+  email: string;
+  commentTime: string;
+}
+const UserReviewCard: React.FC<UserReviewCard> = (props) => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col sm={2}>
+          <Image width={'100%'} className="img-fluid " src={userPlaceHolder}/>
+        </Col>
+        <Col sm={10}>
+          <Card className="card-div-fluid p-2 mb-3">
+            <ReactStars
+              className="d-flex m-5"
+              count={6}
+              size={35}
+              value={props.rating *.6}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p>{props.comment}</p>
+            <span className="supper-small-txt">{props.email}</span>
+            <small className="supper-small-txt">{props.commentTime}</small>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+export default UserReviewCard;
